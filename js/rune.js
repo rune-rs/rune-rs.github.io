9223773cae216da685fd62526eec865988acbf73
tgolsson <tgolsson@users.noreply.github.com>
2020-12-16 19:49:59 +0000

    deploy: rune-rs/rune@3b87ded211d3d88871f4d8ee265f3a1d200f89f6

--- js/rune.js
+++ js/rune.js
@@ -484,7 +484,7 @@ var rune = (function (exports) {
   });
 
   var wasm$1 = async () => {
-                          await init("/js/assets/rune-wasm-b9f772f7.wasm");
+                          await init("/js/assets/rune-wasm-285e17ab.wasm");
                           return exports$1;
                       };
 
