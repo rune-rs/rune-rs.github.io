da4bfac05418be62fb896133a877afa476e969f1
udoprog <udoprog@users.noreply.github.com>
2021-11-16 08:09:34 +0000

    deploy: rune-rs/rune@296114d321cc4230997c1de8761e8b3200d05bd8

--- js/rune.js
+++ js/rune.js
@@ -521,7 +521,7 @@ var rune = (function (exports) {
   });
 
   var wasm$1 = async () => {
-                          await init("/js/assets/rune-wasm-bfb82509.wasm");
+                          await init("/js/assets/rune-wasm-ea6d16f5.wasm");
                           return exports$1;
                       };
 
