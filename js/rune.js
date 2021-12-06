049e95e6e0560d958f479af6eeeec6ce956148fa
udoprog <udoprog@users.noreply.github.com>
2021-12-06 04:41:41 +0000

    deploy: rune-rs/rune@f268b0f7a65171acebb415579ecaea3419ee3179

--- js/rune.js
+++ js/rune.js
@@ -523,7 +523,7 @@ var rune = (function (exports) {
   var wasm = async (opt = {}) => {
                           let {importHook, serverPath} = opt;
 
-                          let path = "/js/assets/rune-wasm-bdf85f13.wasm";
+                          let path = "/js/assets/rune-wasm-ffe8a939.wasm";
 
                           if (serverPath != null) {
                               path = serverPath + /[^\/\\]*$/.exec(path)[0];
