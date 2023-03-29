eaaa323cfb5e68e788911cf33848f399f4652b2c
udoprog <udoprog@users.noreply.github.com>
2023-03-29 19:52:32 +0000

    deploy: rune-rs/rune@ea498b217756e420d5e0ffcd08d057691dcba05d

--- js/rune.js
+++ js/rune.js
@@ -426,7 +426,7 @@ var rune = (function (exports) {
           const ret = wasm$1.memory;
           return addHeapObject(ret);
       };
-      imports.wbg.__wbindgen_closure_wrapper453 = function(arg0, arg1, arg2) {
+      imports.wbg.__wbindgen_closure_wrapper455 = function(arg0, arg1, arg2) {
           const ret = makeMutClosure(arg0, arg1, 234, __wbg_adapter_24);
           return addHeapObject(ret);
       };
@@ -479,7 +479,7 @@ var rune = (function (exports) {
   var wasm = async (opt = {}) => {
                   let {importHook, serverPath} = opt;
 
-                  let path = "/js/assets/rune_wasm-58d64487.wasm";
+                  let path = "/js/assets/rune_wasm-ca213a72.wasm";
 
                   if (serverPath != null) {
                       path = serverPath + /[^\/\\]*$/.exec(path)[0];
