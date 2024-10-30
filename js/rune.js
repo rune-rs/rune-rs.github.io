bf253a03874ec96dbd73fa67fb30c9c6b86f2d75
udoprog <udoprog@users.noreply.github.com>
2024-10-30 18:49:09 +0000

    deploy: rune-rs/rune@2210843141dc1d5200b209fc72d67c5a4a55a3a8

--- js/rune.js
+++ js/rune.js
@@ -130,7 +130,7 @@ var rune = (function (exports) {
       return real;
   }
   function __wbg_adapter_26(arg0, arg1, arg2) {
-      wasm$1.closure1025_externref_shim(arg0, arg1, arg2);
+      wasm$1.closure1024_externref_shim(arg0, arg1, arg2);
   }
 
   /**
@@ -412,8 +412,8 @@ var rune = (function (exports) {
           return ret;
       };
       imports.wbg.__wbg_queueMicrotask_c5419c06eab41e73 = typeof queueMicrotask == 'function' ? queueMicrotask : notDefined('queueMicrotask');
-      imports.wbg.__wbindgen_closure_wrapper5824 = function(arg0, arg1, arg2) {
-          const ret = makeMutClosure(arg0, arg1, 1026, __wbg_adapter_26);
+      imports.wbg.__wbindgen_closure_wrapper5831 = function(arg0, arg1, arg2) {
+          const ret = makeMutClosure(arg0, arg1, 1025, __wbg_adapter_26);
           return ret;
       };
       imports.wbg.__wbindgen_init_externref_table = function() {
@@ -498,7 +498,7 @@ var rune = (function (exports) {
   var wasm = async (opt = {}) => {
                   let {importHook, serverPath} = opt;
 
-                  let path = "/js/assets/rune_wasm-ff98a28b.wasm";
+                  let path = "/js/assets/rune_wasm-c1430942.wasm";
 
                   if (serverPath != null) {
                       path = serverPath + /[^\/\\]*$/.exec(path)[0];
