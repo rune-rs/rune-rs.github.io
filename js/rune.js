9e1ce26c864e73451e3c797808462248cb1f636d
udoprog <udoprog@users.noreply.github.com>
2024-11-03 18:36:45 +0000

    deploy: rune-rs/rune@6b31eda03d468648de84de4511ce724985dece27

--- js/rune.js
+++ js/rune.js
@@ -130,7 +130,7 @@ var rune = (function (exports) {
       return real;
   }
   function __wbg_adapter_26(arg0, arg1, arg2) {
-      wasm$1.closure1218_externref_shim(arg0, arg1, arg2);
+      wasm$1.closure1213_externref_shim(arg0, arg1, arg2);
   }
 
   /**
@@ -160,7 +160,7 @@ var rune = (function (exports) {
       }
   }
   function __wbg_adapter_60(arg0, arg1, arg2, arg3) {
-      wasm$1.closure260_externref_shim(arg0, arg1, arg2, arg3);
+      wasm$1.closure261_externref_shim(arg0, arg1, arg2, arg3);
   }
 
   function notDefined(what) { return () => { throw new Error(`${what} is not defined`); }; }
@@ -412,8 +412,8 @@ var rune = (function (exports) {
           return ret;
       };
       imports.wbg.__wbg_queueMicrotask_c5419c06eab41e73 = typeof queueMicrotask == 'function' ? queueMicrotask : notDefined('queueMicrotask');
-      imports.wbg.__wbindgen_closure_wrapper6236 = function(arg0, arg1, arg2) {
-          const ret = makeMutClosure(arg0, arg1, 1219, __wbg_adapter_26);
+      imports.wbg.__wbindgen_closure_wrapper6183 = function(arg0, arg1, arg2) {
+          const ret = makeMutClosure(arg0, arg1, 1214, __wbg_adapter_26);
           return ret;
       };
       imports.wbg.__wbindgen_init_externref_table = function() {
@@ -498,7 +498,7 @@ var rune = (function (exports) {
   var wasm = async (opt = {}) => {
                   let {importHook, serverPath} = opt;
 
-                  let path = "/js/assets/rune_wasm-6286f3a2.wasm";
+                  let path = "/js/assets/rune_wasm-959c0bc2.wasm";
 
                   if (serverPath != null) {
                       path = serverPath + /[^\/\\]*$/.exec(path)[0];
