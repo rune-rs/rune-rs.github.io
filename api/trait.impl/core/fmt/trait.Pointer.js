2273f7a1d32210d3b7c26db0e35251e68dc1fdf1
udoprog <udoprog@users.noreply.github.com>
2025-06-05 09:25:22 +0000

    deploy: rune-rs/rune@f23ab8fefda46d7a7d0ce8c3388a64602300bd89

--- api/trait.impl/core/fmt/trait.Pointer.js
+++ api/trait.impl/core/fmt/trait.Pointer.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["portable_atomic",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"portable_atomic/struct.AtomicPtr.html\" title=\"struct portable_atomic::AtomicPtr\">AtomicPtr</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["portable_atomic",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"portable_atomic/struct.AtomicPtr.html\" title=\"struct portable_atomic::AtomicPtr\">AtomicPtr</a>&lt;T&gt;"]]],["rune_alloc",[["impl&lt;T, A&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"rune_alloc/sync/struct.Arc.html\" title=\"struct rune_alloc::sync::Arc\">Arc</a>&lt;T, A&gt;<div class=\"where\">where\n    T: ?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>,\n    A: <a class=\"trait\" href=\"rune_alloc/alloc/trait.Allocator.html\" title=\"trait rune_alloc::alloc::Allocator\">Allocator</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[314]}
+//{"start":57,"fragment_lengths":[314,612]}
